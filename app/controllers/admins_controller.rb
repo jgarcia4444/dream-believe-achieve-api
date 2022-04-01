7e0a94bd1f4451ea6383f3f42bf69f035a4a2d14
Jake Garcia <j.garcia@idreamers.com>
2022-03-31 19:28:42 -0700

    Added more conditional logic to the admin_create action.

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,24 +1,79 @@
 
 class AdminsController < ApplicationController
     def create
-        creating_admin = params[:creating_admin]
-        if creating_admin
-            new_admin = Admin.create(admin_params)
-            if new_admin.valid?
-                render :json => {
-                    error: {
-                        hasError: false
-                    },
-                    new_admin: {
-                        username: new_admin.username
+        if params[:admin_create]
+            admin_create = params[:admin_create]
+            creating_admin_info = admin_create[:creating_admin]
+            if creating_admin_info
+                creating_admin_username = ""
+                if creating_admin_info[:username]
+                    creating_admin_username = creating_admin_info[:username]
+                end
+                creating_admin = Admin.find_by(username: creating_admin_username)
+                if creating_admin
+                    creating_admin_password = ""
+                    if creating_admin_info[:password]
+                        creating_admin_password = creating_admin_info[:password]
+                    end
+                    if creating_admin.authenticate(creating_admin_password)
+                        new_admin_info = admin_create[:new_admin]
+                        if new_admin_info
+                            new_admin_username = ""
+                            if new_admin_info[:username]
+                                new_admin_username = new_admin_info[:username]
+                            end
+                            new_admin_password = ""
+                            if new_admin_info[:password]
+                                new_admin_info = new_admin_info[:password]
+                            end
+                            new_admin = Admin.create(username: new_admin_username, password: new_admin_password)
+                            if new_admin.valid?
+                                render :json => {
+                                    error: {
+                                        hasError: false
+                                    },
+                                    new_admin: {
+                                        username: new_admin.username
+                                    }
+                                }
+                            else
+                                render :json => {
+                                    error: {
+                                        hasError: true,
+                                        message: "There was an error creating the new admin user.",
+                                        errors: new_admin.errors
+                                    }
+                                }
+                            end
+                        else
+                            render :json => {
+                                error: {
+                                    hasError: true,
+                                    message: "New admin info was not sent along."
+                                }
+                            }
+                        end
+                    else
+                        render :json => {
+                            error: {
+                                hasError: true,
+                                message: "Incorrect Password for the creating admin."
+                            }
+                        }
+                    end
+                else
+                    render :json => {
+                        error: {
+                            hasError: true,
+                            message: "No admin found with the given username"
+                        }
                     }
-                }
+                end
             else
                 render :json => {
                     error: {
                         hasError: true,
-                        message: "There was an error creating the new admin user.",
-                        errors: new_admin.errors
+                        message: "Information for a valid creating admin must be sent along to create a new admin user."
                     }
                 }
             end
@@ -26,15 +81,10 @@ class AdminsController < ApplicationController
             render :json => {
                 error: {
                     hasError: true,
-                    message: "Information for a valid creating admin must be sent along to create a new admin user."
+                    message: "Data is being sent improperly."
                 }
             }
         end
     end
-
-    private 
-        def admin_params
-            params.require(:new_admin).permit(:username, :password)
-        end
 
 end
