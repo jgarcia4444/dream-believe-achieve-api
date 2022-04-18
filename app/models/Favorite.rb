05218233dacda56db023db264b31e6f4a985a6e7
Jake Garcia <bballwiz4@gmail.com>
2022-04-18 12:21:16 -0700

    Added to the previous function to only send back ten quotes as a maximum.

--- app/models/Favorite.rb
+++ app/models/Favorite.rb
@@ -5,6 +5,7 @@ class Favorite < ApplicationRecord
     def self.find_top_ten
         favorites = Favorite.all
         sorting_object = {}
+
         favorites.each do |favorite|
             quote_id = favorite.quote_id
             if sorting_object[quote_id]
@@ -13,16 +14,29 @@ class Favorite < ApplicationRecord
                 sorting_object[quote_id] = 1
             end
         end
+
         sorted_quote_ids = sorting_object.keys.sort {|a, b| sorting_object[:b] <=> sorting_object[:a]}
         sorted_quotes = sorted_quote_ids.map {|quote_id| Quote.find_by(id: quote_id)}
-        sorted_quotes.map do |quote|
-            favorites_count = quote.favorites.count
-            {
-                id: quote.id,
-                author: quote.author,
-                quote: quote.quote,
-                favorites: favorites_count
-            }
+        if sorted_quote.count > 10
+            sorted_quotes.slice(0, 10).map do |quote|
+                favorites_count = quote.favorites.count
+                {
+                    id: quote.id,
+                    author: quote.author,
+                    quote: quote.quote,
+                    favorites: favorites_count
+                }
+            end
+        else
+            sorted_quotes.map do |quote|
+                favorites_count = quote.favorites.count
+                {
+                    id: quote.id,
+                    author: quote.author,
+                    quote: quote.quote,
+                    favorites: favorites_count
+                }
+            end
         end
     end
 
