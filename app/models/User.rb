862054f73b22c7a1218c194c2ad8441900fce241
Jake Garcia <bballwiz4@gmail.com>
2022-04-23 19:02:17 -0700

    Removed puts

--- app/models/User.rb
+++ app/models/User.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     validates :username, uniqueness: true
     validates :username, presence: true
     validates :password_digest, presence: true
+    validates :password_digest, length: {minimum: 8}
 
     def add_quote_to_daily_quotes(quote)
         new_daily_quote = DailyQuote.create(quote_id: quote.id, user_id: self.id)
