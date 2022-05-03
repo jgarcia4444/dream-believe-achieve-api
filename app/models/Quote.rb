3a4516514ad7c6ed0eed822ff7e734fb840e1480
Jake Garcia <bballwiz4@gmail.com>
2022-05-02 21:06:59 -0700

    Fixed the conditional checking if a day has passed.

--- app/models/Quote.rb
+++ app/models/Quote.rb
@@ -2,7 +2,7 @@ class Quote < ApplicationRecord
 
     has_many :favorites, dependent: :destroy
     
-    def filter_quotes(daily_quote_ids)
+    def self.filter_quotes(daily_quote_ids)
         all_quotes = Quote.all
         all_quotes.select {|quote| !daily_quote_ids.any? {|daily_quote_id| daily_quote_id == quote.id}}
     end
